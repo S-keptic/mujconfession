c065beedc51e7f3b950d33f3f2f334d1f0263742
Anmol <anmol.volt@gmail.com>
2023-04-11 22:48:53 +0530

    $

---
+++ src/components/Body.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import  './Body.css'
+
+
+const Body = ()=>{
+return(<>
+
+<p className='para'>Confess your sins in complete anonymity.</p>
+
+
+</>)
+
+
+
+}
+
+export default Body
+
+
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+
+  
+  return (
+    <div className="nav-container">
+      <h1>MUJ-Confession</h1>
+    </div>
+  );
+};
+
+export default Navbar;
+
