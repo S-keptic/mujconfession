71361176111bd2027bb53635216a21b1c5c6f226
Anmol <anmol.volt@gmail.com>
2023-04-11 23:40:29 +0530

    *

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Button.css'
+const Button = ()=>{
+
+    return(<>
+    <div className= "button-container">
+        <button id='bttn' className="btn">Submit</button>
+    </div>
+
+
+
+
+
+
+    </>)
+}
+
+export default Button
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import Navbar from './components/Navbar';
 import './index.css'
 import Body from './components/Body';
 import Textarea from './components/Textarea';
+import Button from './components/Button';
 ReactDOM.render(<>
 
 
 <Navbar></Navbar>
 <Body></Body>
 <Textarea></Textarea>
-
+<Button></Button>
 
 
 
